f2f340526dd6df494fe0748c08802d3d93d2fbd1
杨柳 <yangliulnn@163.com>
2020-01-06 13:52:03 +0800

    Update WinicGateway.php
    添加 winic 国际短信支持

--- src/Gateways/WinicGateway.php
+++ src/Gateways/WinicGateway.php
@@ -9,6 +9,7 @@
 
 namespace Leonis\Notifications\EasySms\Gateways;
 
+use Illuminate\Support\Str;
 use Overtrue\EasySms\Contracts\MessageInterface;
 use Overtrue\EasySms\Contracts\PhoneNumberInterface;
 use Overtrue\EasySms\Exceptions\GatewayErrorException;
@@ -21,6 +22,7 @@ class WinicGateway extends Gateway
     use HasHttpRequest;
 
     const ENDPOINT_URL = 'http://service.winic.org:8009/sys_port/gateway/index.asp';
+    const INTERNATIONAL_ENDPOINT_URL = 'http://service2.winic.org/service.asmx/SendInternationalMessages';
 
     /**
      * Send a short message.
@@ -29,11 +31,27 @@ class WinicGateway extends Gateway
      * @param \Overtrue\EasySms\Contracts\MessageInterface     $message
      * @param \Overtrue\EasySms\Support\Config                 $config
      *
-     * @return array
-     *
      * @throws \Overtrue\EasySms\Exceptions\GatewayErrorException
+     * @return array
      */
     public function send(PhoneNumberInterface $to, MessageInterface $message, Config $config)
+    {
+        if ($to->getIDDCode() === 86) {
+            return $this->mainlandChina($to, $message, $config);
+        } else {
+            return $this->international($to, $message, $config);
+        }
+    }
+
+    /**
+     * @param \Overtrue\EasySms\Contracts\PhoneNumberInterface $to
+     * @param \Overtrue\EasySms\Contracts\MessageInterface     $message
+     * @param \Overtrue\EasySms\Support\Config                 $config
+     *
+     * @throws \Overtrue\EasySms\Exceptions\GatewayErrorException
+     * @return array
+     */
+    private function mainlandChina(PhoneNumberInterface $to, MessageInterface $message, Config $config): array
     {
         $params = [
             'id' => iconv('utf-8', 'gb2312', $config->get('id')),
@@ -52,5 +70,37 @@ class WinicGateway extends Gateway
         }
 
         return $result;
+    }
+
+    /**
+     * @param \Overtrue\EasySms\Contracts\PhoneNumberInterface $to
+     * @param \Overtrue\EasySms\Contracts\MessageInterface     $message
+     * @param \Overtrue\EasySms\Support\Config                 $config
+     *
+     * @throws \Overtrue\EasySms\Exceptions\GatewayErrorException
+     * @return array
+     */
+    private function international(PhoneNumberInterface $to, MessageInterface $message, Config $config): array
+    {
+        $params = [
+            'uid' => $config->get('id'),
+            'pwd' => $config->get('pwd'),
+            'tos' => $to->getIDDCode() . $to->getNumber(),
+            'msg' => $message->getContent(),
+            'otime' => '',
+        ];
+
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+        ];
+
+        /** @var string $response */
+        $response = $this->post(self::INTERNATIONAL_ENDPOINT_URL, $params, $headers);
+
+        if (Str::contains($response[0], '-')) {
+            throw new GatewayErrorException($response, $response[0], $response);
+        }
+
+        return $response;
     }
 }
